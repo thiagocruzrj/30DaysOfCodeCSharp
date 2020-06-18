291e8f31d577d7157084771e6f061a8e652201d0
Thiago Cruz <thagocruz@gmail.com>
2020-06-18 00:38:58 -0300

    Create Day8.cs

---
+++ Day8.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using static System.Console;
+
+class Day8
+{
+    static void Main(String[] args)
+    {
+        Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+
+        int entries = Int32.Parse(Console.ReadLine());
+
+        for (int i = 0; i < entries; i++)
+        {
+            string[] entry = Console.ReadLine().Split(' ');
+            phoneBook.Add(entry[0], entry[1]);
+        }
+
+        string name = Console.ReadLine();
+
+        while (!string.IsNullOrEmpty(name))
+        {
+            if (phoneBook.TryGetValue(name, out string phoneNumber))
+            {
+                Console.WriteLine("{0}={1}", name, phoneNumber);
+            }
+            else
+            {
+                Console.WriteLine("Not found");
+            }
+
+            name = Console.ReadLine();
+        }
+    }
+}
