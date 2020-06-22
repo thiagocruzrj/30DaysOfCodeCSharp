db41ae72b3c4ebcc329afe5cc89cc55a1667ed52
Thiago Cruz <thagocruz@gmail.com>
2020-06-22 11:36:30 -0300

    Change old days

--- Day4.cs
+++ Day4.cs
@@ -5,7 +5,7 @@ namespace DaysOfCodeCSharp
     class Day4 
     {
         public int age;     
-	    public Person(int initialAge) 
+	    public void Person(int initialAge) 
         {
             if (initialAge > 0)
             {
@@ -18,17 +18,17 @@ namespace DaysOfCodeCSharp
             }
         }
 
-         public void amIOld() 
+        public void amIOld() 
         {
             if (age < 13) Console.WriteLine("You are young.");
             else if (age < 18) Console.WriteLine("You are a teenager.");
             else Console.WriteLine("You are old.");
-         }
+        }
 
-         public void yearPasses() 
-         {
+        public void yearPasses() 
+        {
             age++;
-         }
+        }
 
         static void Main(String[] args) 
         {
@@ -36,8 +36,8 @@ namespace DaysOfCodeCSharp
             for (int i = 0; i < T; i++) 
             {
                 int age=int.Parse(Console.In.ReadLine());
-                Person p=new Person(age);
-                 p.amIOld();
+                Person p = new Person(age);
+                p.amIOld();
 
                 for (int j = 0; j < 3; j++) 
                 {
