f1d31fb0a974fcd2eb9bab410930cf1eaaffcfc5
Thiago Cruz <thagocruz@gmail.com>
2020-07-18 16:12:19 -0300

    Create Day20.cs

---
+++ Day20.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void swap(ref int a, ref int b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    static void Main(String[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] a_temp = Console.ReadLine().Split(' ');
+        int[] a = Array.ConvertAll(a_temp,Int32.Parse);
+        int numberOfSwaps = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n - 1; j++)
+            {
+                if (a[j] > a[j + 1])
+                {
+                    swap(ref a[j], ref a[j + 1]);
+                    numberOfSwaps++;
+                }
+            }
+
+            if (numberOfSwaps == 0)
+            {
+                break;
+            }
+        }
+        Console.WriteLine($"Array is sorted in {numberOfSwaps} swaps. ");
+        Console.WriteLine($"First Element: {a[0]}");
+        Console.WriteLine($"Last Element: {a[a.Length - 1]}");
+    }
+}
